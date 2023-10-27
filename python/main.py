c5658236e2431f55bdf4a87602f6c1ebefad5c29
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-10-26 21:39:14 -0300

    exercicio Formulario

---
+++ python/main.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu ultimo sobrenome: ')
+cpf = input('Digite seu CPF: ')
+endereco = input('Digite seu endereço: ')
+idade = input('Digite sua idade: ')
+altura = input('Digite sua altura: ')
+telefone = input('Digite seu telefone: ')
+
+print('Cadastro do Senhor', nome, 'concluído com sucesso!', '\n' 'Senhor', nome, sobrenome,'\n' 'CPF:', cpf,'\n' 'Idade:', idade, 'anos','\n' 'Altura:', altura,'\n' 'Telefone:', telefone,'\n' 'Residente no endereço:', endereco)
