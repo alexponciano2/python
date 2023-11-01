aefd215dc0b0dffdf06c1030e1afdd06ec536ed0
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-10-31 21:30:59 -0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,9 +1,32 @@
-nome = input('Digite seu nome: ')
-sobrenome = input('Digite seu ultimo sobrenome: ')
-cpf = input('Digite seu CPF: ')
-endereco = input('Digite seu endereço: ')
-idade = input('Digite sua idade: ')
-altura = input('Digite sua altura: ')
-telefone = input('Digite seu telefone: ')
+# nome = input('Digite seu nome: ')
+# sobrenome = input('Digite seu ultimo sobrenome: ')
+# cpf = input('Digite seu CPF: ')
+# endereco = input('Digite seu endereço: ')
+# idade = input('Digite sua idade: ')
+# altura = input('Digite sua altura: ')
+# telefone = input('Digite seu telefone: ')
 
-print('Cadastro do Senhor', nome, 'concluído com sucesso!', '\n' 'Senhor', nome, sobrenome,'\n' 'CPF:', cpf,'\n' 'Idade:', idade, 'anos','\n' 'Altura:', altura,'\n' 'Telefone:', telefone,'\n' 'Residente no endereço:', endereco)
+# print('Cadastro do Senhor', nome, 'concluído com sucesso!', '\n' 'Senhor', nome, sobrenome,'\n' 'CPF:', cpf,'\n' 'Idade:', idade, 'anos','\n' 'Altura:', altura,'\n' 'Telefone:', telefone,'\n' 'Residente no endereço:', endereco)
+# ------------------------------------------------------------------------
+# a = 50
+# b = 30
+# fruta = input("digite uma fruta inciado com a letra A: ")
+
+# if a > b and fruta == 'abacaxi':
+#     print(a, "é maior do que", b, "e a fruta é", fruta)
+# else:
+#     print("deu ruim teu IF")
+# ------------------------------------------------------------------------
+print('MENU: \n1 = Escreve Guilherme\n2 = Escreve João\n3 = Escreve Maria\n')
+
+opcao = input("Escolha uma opção: ")
+
+if opcao == '1':
+    print('Você escolheu: Guilherme\n')
+elif opcao == '2':
+    print('Você escolheu: João\n')
+elif opcao == '3':
+    print('Você escolheu: Maria\n')
+else:
+    print('Opção inválida!\n')
+# ------------------------------------------------------------------------
