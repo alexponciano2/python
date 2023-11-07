d9df1086dbcefb4c367be0bd1494ebe10998b53c
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-11-06 21:37:14 -0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -39,12 +39,53 @@
 # else:
 #     print("Não pode entrar no exército!")
 # ------------------------------------------------------------------------
-lista_nomes = ['João', 'Maria', 'Guilherme', 'Diego']
+# lista_nomes = ['João', 'Maria', 'Guilherme', 'Diego']
 
-lista_nomes.append('ALEX')
-lista_nomes.remove('Maria')
-lista_nomes.insert(1, 'Helena')
+# lista_nomes.append('ALEX')
+# lista_nomes.remove('Maria')
+# lista_nomes.insert(1, 'Helena')
 
 # contador_Alex = lista_nomes.count('ALEX')
 
-print(lista_nomes.lower())
+# print(lista_nomes.lower())
+# ------------------------------------------------------------------------
+# nomes = ['Guilherme', 'Marcelo', 'João', 'Júlia']
+
+# for i in nomes:
+#     print(i)
+# ------------------------------------------------------------------------
+# nomes = ['Guilherme', 'Marcelo', 'João', 'Júlia']
+
+# for i in range(len(nomes)):
+#     print(nomes[i])
+# ------------------------------------------------------------------------
+# palavra = 'Guilherme Junqueira'
+
+# for i in palavra:
+#     print(i)
+# ------------------------------------------------------------------------
+# i = 0
+# while i < 10:
+#     print('i ainda é menor do que 10: ', i)
+#     i = i + 1
+
+# print('Acabou o While, seu "i" agora vale:', i)
+# ------------------------------------------------------------------------
+# numero = 0
+# while True:
+#     print(numero)
+#     if numero == 20:
+#         break
+#     numero += 1
+# ------------------------------------------------------------------------
+# EXERCÍCIO:
+
+quantidade_de_pessoas = int(input('Quantas pessoas virão à festa?: '))
+
+lista_de_convidados = []
+
+for i in range(quantidade_de_pessoas):
+    nome = input('Qual o nome do convidado?: ')
+    lista_de_convidados.append(nome)
+   
+print('Os convidados para a festa são: ', lista_de_convidados)
